93ded59638d90382c70c1e30756b60bb4adae98b
juan <juansvrs0321@gmail.com>
2023-05-04 12:35:31 -0500

    Subido el repositorio con hola mundo y parte de las variables

---
+++ variables/variables.go
@@ -0,0 +1,11 @@
+package variables
+
+func main() {
+	/* GO nos obliga a utilizar las variables 
+	porque sino, ocupamos memoria, por eso sale error*/
+	var nombre1 string
+	nombre1 = "Juan Luis"
+
+	var nombre2 string = "Roel"
+
+}
